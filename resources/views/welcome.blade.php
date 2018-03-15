519281744cfce056b898aecfbeaf453ce2641af3
gimmy <jychri@gmail.com>
2018-03-15 17:45:19 +0100

    Home2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,87 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">welcome</div>
 
-                <div class="card-body text-center">
-                  Team Pitcher App
+
+
+                <!-- Page Heading -->
+                <h1 class="my-4">Page Heading
+                  <small>Secondary Text</small>
+                </h1>
+
+                <div class="row">
+                  <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                      <h4 class="card-title">
+                        <a href="#">Project One</a>
+                      </h4>
+                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                      <div class="card-body">
+                        <p class="card-text"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                      <h4 class="card-title">
+                        <a href="#">Project Two</a>
+                      </h4>
+                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                      <div class="card-body">
+                        <p class="card-text"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                      <h4 class="card-title">
+                        <a href="#">Project Three</a>
+                      </h4>
+                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                      <div class="card-body">
+                        <p class="card-text"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                      <h4 class="card-title">
+                        <a href="#">Project Four</a>
+                      </h4>
+                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                      <div class="card-body">
+                        <p class="card-text"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                      <h4 class="card-title">
+                        <a href="#">Project Five</a>
+                      </h4>
+                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                      <div class="card-body">
+                        <p class="card-text"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-lg-4 col-sm-6 portfolio-item">
+                    <div class="card h-100">
+                      <h4 class="card-title">
+                        <a href="#">Project Six</a>
+                      </h4>
+                      <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                      <div class="card-body">
+                        <p class="card-text"></p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
+                <!-- /.row -->
+
+
+
+
             </div>
         </div>
     </div>
