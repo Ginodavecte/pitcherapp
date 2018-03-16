ec76730305e18018b6286090c0d2a1bb17af6648
JvP <jvp@live.nl>
2018-03-16 09:22:00 +0100

    category page setup

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/categories', 'CategoriesController');
+Route::resource('/products', 'ProductsController');
