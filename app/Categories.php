05cf5291f5a3b61860308023eb4a970450768673
JvP <jvp@live.nl>
2018-03-16 16:51:39 +0100

    Category page

--- app/Categories.php
+++ app/Categories.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    $fillable = ['name'];
-    
+  protected $fillable = ['name'];
+
 }
---
+++ public/js/categories.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $("#block").click(function(){
+    $("#product_list").hide(1000);
+    $("#product_block").show(1000);
+  });
+
+  $("#list").click(function(){
+    $("#product_list").show(1000);
+    $("#product_block").hide(1000);
+  });
+  $( function() {
+      $( "#slider-range" ).slider({
+        range: true,
+        min: 0,
+        max: 500,
+        values: [ 75, 300 ],
+        slide: function( event, ui ) {
+          $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        }
+      });
+      $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+        " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+    } );
+
+
+});
