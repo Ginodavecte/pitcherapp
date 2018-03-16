8b8ab8cb84f3d37b0c154fa18111934a4873b7eb
JvP <jvp@live.nl>
2018-03-16 16:51:39 +0100

    Category page

--- app/subcategory.php
+++ app/subcategory.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class subcategory extends Model
 {
+<<<<<<< c3c6b380562e30b26792088049d00efea40a44aa:app/subcategory.php
   
+=======
+  protected $fillable = ['name'];
+>>>>>>> Category page:app/Categories.php
 
 }
--- public/js/categories.js
+++ public/js/categories.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
     $("#product_list").show(1000);
     $("#product_block").hide(1000);
   });
-<<<<<<< 5bfe158a3ec017f6786688a0b9f659de8ca9c1e3
 
 
 
@@ -24,39 +23,3 @@ $(document).ready(function(){
     }
 
   });
-=======
-  $( function() {
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 500,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-  } );
-
-  $( function() {
-    $( "#tabs" ).tabs({
-      event: "mouseover"
-    });
-  } );
-/* Product filter */
-  $(".button").click(function(){
-    var value = $(this).attr("data-filter");
-    if(value == "all"){
-      $(".filter").show(1000);
-    }else{
-      $(".filter").not("."+value).hide(1000);
-      $(".filter").filter("."+value).show(1000);
-    }
-
-<<<<<<< 4b9bbc21432b64bbbe43f3a329cd4c4ed9a12f6f
->>>>>>> Category page
-=======
-  });
->>>>>>> added product filtering to the category page
-});
