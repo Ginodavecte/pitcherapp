2fd184cfa7d702c58a52826d5870bb4829245929
gimmy <jychri@gmail.com>
2018-03-19 19:40:46 +0100

    PB-1

--- resources/views/billingdata.blade.php
+++ resources/views/billingdata.blade.php
@@ -14,11 +14,11 @@
     <div class="row cart-head">
       <div class="container">
         <ol class="progtrckr" data-progtrckr-steps="5">
-            <li class="progtrckr-doing">ShopCart</li><!--
-         --><li class="progtrckr-doing">Billing</li><!--
-         --><li class="progtrckr-todo">Payment</li><!--
-         --><li class="progtrckr-todo">Place Order</li><!--
-         --><li class="progtrckr-todo">Leave Review</li>
+            <li class="progtrckr-doing">@lang('messages.shopcart')</li><!--
+         --><li class="progtrckr-doing">@lang('messages.billing')</li><!--
+         --><li class="progtrckr-todo">@lang('messages.payment')</li><!--
+         --><li class="progtrckr-todo">@lang('messages.place_order')</li><!--
+         --><li class="progtrckr-todo">@lang('messages.leave_review')</li>
         </ol>
         <br>
       </div>
@@ -30,65 +30,65 @@
           <div class="col-md-6">
             <!--SHIPPING METHOD-->
             <div class="card" style="border-color: #999;">
-              <div class="card-header" style="color: #eee; background-image: linear-gradient(to bottom, #555 0px, #888 100%);">Address</div>
+              <div class="card-header" style="color: #eee; background-image: linear-gradient(to bottom, #555 0px, #888 100%);">@lang('messages.address')</div>
               <!-- <div class="card-header bg-secondary">Address</div> -->
               <div class="card-body">
                 <div class="form-group">
                   <div class="form-group">
-                    <h4>Shipping Address</h4>
+                    <h4>@lang('messages.shipping_address')</h4>
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>Country:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.country:')</strong></div>
                   <div class="form-group">
                     <input type="text" class="form-control" name="country" value="" />
                   </div>
                 </div>
                 <div class="form-row">
                   <div class="col">
-                    <strong>First Name:</strong>
+                    <strong>@lang('messages.first_name:')</strong>
                     <input type="text" name="first_name" class="form-control" value="" />
                   </div>
 
                   <div class="col">
-                    <strong>Last Name:</strong>
+                    <strong>@lang('messages.last_name:')</strong>
                     <input type="text" name="last_name" class="form-control" value="" />
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>Address:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.address:')</strong></div>
                   <div class="form-group">
                     <input type="text" name="address" class="form-control" value="" />
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>City:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.city:')</strong></div>
                   <div class="form-group">
                     <input type="text" name="city" class="form-control" value="" />
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>State:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.state:')</strong></div>
                   <div class="form-group">
                     <input type="text" name="state" class="form-control" value="" />
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>Zip / Postal Code:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.zip_postal_code:')</strong></div>
                   <div class="form-group">
                     <input type="text" name="zip_code" class="form-control" value="" />
                   </div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>Phone Number:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.phone_number:')</strong></div>
                   <div class="form-group"><input type="text" name="phone_number" class="form-control" value="" /></div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>Email Address:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.email_address:')</strong></div>
                   <div class="form-group"><input type="text" name="email_address" class="form-control" value="" /></div>
                 </div>
                 <div class="form-group">
-                  <div class="form-group"><strong>Shipping Method:</strong></div>
+                  <div class="form-group"><strong>@lang('messages.shipping_method:')</strong></div>
                   <div class="form-group">
                     <div class="radio">
                       <label><input type="radio" name="optradio">PTT</label>
@@ -110,7 +110,7 @@
               <div class="card-header" style="color: #eee; background-image: linear-gradient(to bottom, #555 0px, #888 100%);"><span><i class="fas fa-lock"></i></span>Secure Payment</div>
               <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item">
-                  <a class="nav-link active" data-toggle="tab" href="#creditcard">Credit Card</a>
+                  <a class="nav-link active" data-toggle="tab" href="#creditcard">@lang('messages.creditcard')</a>
                 </li>
                 <li class="nav-item">
                   <a class="nav-link" data-toggle="tab" href="#paypal">Paypal</a>
@@ -125,7 +125,7 @@
                   <div class="card-body">
                     <div class="form-group">
                       <div class="form-group">
-                        <span>Pay secure using your credit card.</span>
+                        <span>@lang('messages.secure_creditcard')</span>
                       </div>
                       <div class="form-group">
 
@@ -143,7 +143,7 @@
                       </div>
                     </div>
                     <div class="form-group">
-                      <div class="form-group"><strong>Card Type:</strong></div>
+                      <div class="form-group"><strong>@lang('messages.cardtype')</strong></div>
                       <div class="form-group">
                         <select id="CreditCardType" name="CreditCardType" class="form-control">
                                             <option value="5">Visa</option>
@@ -154,21 +154,21 @@
                       </div>
                     </div>
                     <div class="form-group">
-                      <div class="form-group"><strong>Credit Card Number:</strong></div>
+                      <div class="form-group"><strong>@lang('messages.creditcardnumber')</strong></div>
                       <div class="form-group"><input type="text" class="form-control" name="car_number" value="" /></div>
                     </div>
                     <div class="form-group">
-                      <div class="form-group"><strong>Card CVV:</strong></div>
+                      <div class="form-group"><strong>@lang('messages.cardcvv')</strong></div>
                       <div class="form-group"><input type="text" class="form-control" name="car_code" value="" /></div>
                     </div>
                     <div class="form-group">
                       <div class="form-group">
-                        <strong>Expiration Date</strong>
+                        <strong>@lang('messages.expiration_date')</strong>
                       </div>
                       <div class="form-row">
                         <div class="col">
                           <select class="form-control" name="">
-                                            <option value="">Month</option>
+                                            <option value="">@lang('messages.month')</option>
                                             <option value="01">01</option>
                                             <option value="02">02</option>
                                             <option value="03">03</option>
@@ -185,7 +185,7 @@
                         </div>
                         <div class="col">
                           <select class="form-control" name="">
-                                            <option value="">Year</option>
+                                            <option value="">@lang('messages.year')</option>
                                             <option value="2015">2015</option>
                                             <option value="2016">2016</option>
                                             <option value="2017">2017</option>
@@ -203,7 +203,7 @@
                     </div>
                     <div class="form-group">
                       <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-submit-fix">Place Order</button>
+                        <button type="submit" class="btn btn-primary btn-submit-fix">@lang('messages.place_order')</button>
                       </div>
                     </div>
                   </div>
@@ -224,8 +224,8 @@
             <!--REVIEW ORDER-->
             <div class="card" style="border-color: #999;">
               <div class="card-header" style="color: #eee; background-image: linear-gradient(to bottom, #555 0px, #888 100%);">
-                Review Order
-                <div class="float-right"><small><a class="afix-1" href="#" style="color: #eee";>Edit Cart</a></small></div>
+                @lang('messages.review_order')
+                <div class="float-right"><small><a class="afix-1" href="#" style="color: #eee";>@lang('messages.edit_cart')</a></small></div>
               </div>
               <div class="card-body">
                 <div class="form-group row">
@@ -233,8 +233,8 @@
                     <img class="img-fluid" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
                   </div>
                   <div class="col">
-                    <div class="col">Product name</div>
-                    <div class="col"><small>Quantity:<span>1</span></small></div>
+                    <div class="col">@lang('messages.product_name')</div>
+                    <div class="col"><small>@lang('messages.quantity')<span>1</span></small></div>
                   </div>
                   <div class="col text-right">
                     <h6><span>$</span>25.00</h6>
@@ -248,8 +248,8 @@
                     <img class="img-fluid" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
                   </div>
                   <div class="col">
-                    <div class="col">Product name</div>
-                    <div class="col"><small>Quantity:<span>1</span></small></div>
+                    <div class="col">@lang('messages.product_name')</div>
+                    <div class="col"><small>@lang('messages.quantity')<span>1</span></small></div>
                   </div>
                   <div class="col text-right">
                     <h6><span>$</span>25.00</h6>
@@ -263,8 +263,8 @@
                     <img class="img-fluid" src="//c1.staticflickr.com/1/466/19681864394_c332ae87df_t.jpg" />
                   </div>
                   <div class="col">
-                    <div class="col">Product name</div>
-                    <div class="col"><small>Quantity:<span>2</span></small></div>
+                    <div class="col">@lang('messages.product_name')</div>
+                    <div class="col"><small>@lang('messages.quantity')<span>2</span></small></div>
                   </div>
                   <div class="col text-right">
                     <h6><span>$</span>50.00</h6>
@@ -275,11 +275,11 @@
                 </div>
                 <div class="form-group">
                   <div class="col">
-                    <strong>Subtotal</strong>
+                    <strong>@lang('messages.subtotal')</strong>
                     <div class="float-right"><span>$</span><span>200.00</span></div>
                   </div>
                   <div class="col">
-                    <small>Shipping</small>
+                    <small>@lang('messages.shipping')</small>
                     <div class="float-right"><span>-</span></div>
                   </div>
                 </div>
@@ -288,7 +288,7 @@
                 </div>
                 <div class="form-group">
                   <div class="col">
-                    <strong>Order Total</strong>
+                    <strong>@lang('messages.order_total')</strong>
                     <div class="float-right"><span>$</span><span>150.00</span></div>
                   </div>
 
@@ -316,10 +316,10 @@
       <div class="col mb-2">
           <div class="row">
               <div class="col-sm-12  col-md-6">
-                  <button class="btn btn-block btn-light">Continue Shopping</button>
+                  <button class="btn btn-block btn-light">@lang('messages.continue_shopping')</button>
               </div>
               <div class="col-sm-12 col-md-6 text-right">
-                  <button class="btn btn-lg btn-block btn-success text-uppercase">Next Step (PAYMENT)</button>
+                  <button class="btn btn-lg btn-block btn-success text-uppercase">@lang('messages.next_step_payment')</button>
               </div>
           </div>
       </div>
