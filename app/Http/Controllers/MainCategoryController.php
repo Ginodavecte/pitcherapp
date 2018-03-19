cc4a23760988f26580d9fcf8b01400e2ca8f20fb
JvP <jvp@live.nl>
2018-03-19 17:56:32 +0100

    Migrations setup

--- app/Http/Controllers/MainCategoryController.php
+++ app/Http/Controllers/MainCategoryController.php
@@ -48,9 +48,9 @@ class MainCategoryController extends Controller
      * @param  \App\main_category  $main_category
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(main_category $main_category)
     {
-       //
+        //
     }
 
     /**
--- app/subcategory.php
+++ app/subcategory.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class subcategory extends Model
 {
-<<<<<<< c3c6b380562e30b26792088049d00efea40a44aa:app/subcategory.php
-  
-=======
   protected $fillable = ['name'];
->>>>>>> Category page:app/Categories.php
-
 }
