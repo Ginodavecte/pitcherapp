0d2554cdf76cd40cbad779d4aa6274d2caefb7eb
JvP <jvp@live.nl>
2018-03-20 12:54:42 +0100

    migrations

--- app/Maincategory.php
+++ app/Maincategory.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Maincategory extends Model
 {
+  protected $fillable = ['name'];
+  
   public function products(){
     return $this->belongsToMany('App\Product');
   }
