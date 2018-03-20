9e563c0120a18f1f09d6361a43cb10b4e3511314
JvP <jvp@live.nl>
2018-03-20 09:20:11 +0100

    Migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-	$this->call(TableProductSeeder::class);
+	      $this->call(ProductTableSeeder::class);
       	$this->call(MainCategoriesTableSeeder::class);
     }
 }
