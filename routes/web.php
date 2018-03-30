fed629653dc1575750926bb47ffb6d31f6e91e60
gimmy <jychri@gmail.com>
2018-03-30 18:43:02 +0200

    U-3

--- routes/web.php
+++ routes/web.php
@@ -53,3 +53,6 @@ Route::get('/wishlist', function () {
 Route::get('/orders', function () {
     return view('/account/orders');
 });
+Route::get('/wishlist', function () {
+    return view('/account/wishlist');
+});
